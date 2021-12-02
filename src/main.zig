0d25e38307a638ccbe21836d5c870e3dce7abf53
Robert Hailey <git@osndok.com>
2021-12-01 21:38:01 -0600

    Waypoint: it logs arguments

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
+const log = std.log;
+const out = std.io.getStdOut().writer();
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub fn main() anyerror!void
+{
+    const allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    //???: defer std.debug.assert(!allocator.deinit());
+
+    //var i = std.process.args();
+    
+    //while (i.hasNext())
+    {
+        //var n = i.next(allocator);
+        
+    }
+    
+    for (std.os.argv) |a, i|
+    {
+        if (i > 0)
+        {
+            log.info("arg: {s}", .{a});
+        }
+    }
 }
