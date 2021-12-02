c90cb7c3017604f932e9b0d4c39f26492da8f9c7
Robert Hailey <git@osndok.com>
2021-12-01 22:03:31 -0600

    Success: cmdlet now behaves as intended

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,14 @@
 const std = @import("std");
-const log = std.log;
 const out = std.io.getStdOut().writer();
 
 pub fn main() anyerror!void
 {
-    const allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    //???: defer std.debug.assert(!allocator.deinit());
-
-    //var i = std.process.args();
-    
-    //while (i.hasNext())
-    {
-        //var n = i.next(allocator);
-        
-    }
-    
     for (std.os.argv) |a, i|
     {
         if (i > 0)
         {
-            log.info("arg: {s}", .{a});
+            try out.print("{s}", .{a});
+            try out.writeByte(0);
         }
     }
 }
